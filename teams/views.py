06dbe962d3fd0a37c0b8f5b311a650008aad8711
Toshka <tinkerino571@gmail.com>
2024-06-28 15:44:51 +0300

    Pagination added

--- teams/views.py
+++ teams/views.py
@@ -1,15 +1,21 @@
-from rest_framework import generics
+from rest_framework import generics, pagination
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Team, Person
 from .serializers import TeamSerializer, PersonSerializer
 from .filters import TeamFilter, PersonFilter
 
+class StandardResultsSetPagination(pagination.PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
 class TeamListCreate(generics.ListCreateAPIView):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = TeamFilter
+    pagination_class = StandardResultsSetPagination
 
 class TeamDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Team.objects.all()
@@ -20,6 +26,7 @@ class PersonListCreate(generics.ListCreateAPIView):
     serializer_class = PersonSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = PersonFilter
+    pagination_class = StandardResultsSetPagination
 
 class PersonDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Person.objects.all()
